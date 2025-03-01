1c3735c7b93818ececb9b392d46134f4790fe8ca
Bader Altabba <baderaltabba247@gmail.com>
2025-03-01 14:23:43 +0300

    footer

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -1,20 +1,38 @@
-import React ,{Suspense} from "react";
+import React ,{Suspense,useState} from "react";
 import useSWR from 'swr';
 import Apiconfigs from "src/Apiconfig/Apiconfigs";
+import { Dialog, DialogTitle, DialogContent, DialogContentText } from "@mui/material";
 
 import { CgFacebook } from "react-icons/cg";
-import { FaTelegram, FaTwitterSquare, FaYoutube } from "react-icons/fa";
+import { FaTelegram, FaTwitterSquare, FaDiscord, FaFacebook } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai"; // Close icon from Ant Design
+
 import { SiAppstore, SiGoogleplay } from "react-icons/si";
 import './footer.css';
 import Container from '@mui/material/Container'
 import { Link } from "react-router-dom";
 import axios from "axios";
+import NoDataFound from "src/component/NoDataFound";
+
 
 const Footer = () => {
 const fetcher = url => axios.get(url).then(res => res.data.result);
 const { data: staticContent } = useSWR(Apiconfigs.staticContentList, fetcher, { suspense: true })
 const { data: socialLinks } = useSWR(Apiconfigs.listSocial, fetcher, { suspense: true })
-console.log("heeeee",socialLinks)
+const [open, setOpen] = useState(false);
+const [selectedItem, setSelectedItem] = useState(null);
+
+const handleClickOpen = (item) => {
+  setSelectedItem(item);
+  setOpen(true);
+};
+
+const handleClose = () => {
+  setOpen(false);
+  setSelectedItem(null);
+};
+
+console.log("heeeee",staticContent)
   return (
     <footer
       style={{
@@ -24,6 +42,7 @@ console.log("heeeee",socialLinks)
         color: "white",
         textAlign: "center",
         fontFamily: "'Arial', sans-serif",
+        
       }}
       className="footer"
     >
@@ -44,43 +63,46 @@ console.log("heeeee",socialLinks)
             src="\assets\Images\masfooter-logo.svg"
             alt="Logo"
             style={{
-              width: "120px",
-              height: "120px",
+              width: "150px",
+              height: "150px",
               
             }}
           />
           </Link> 
-          <h3 style={{ fontSize: "24px", fontWeight: "bold" }}>MAS Platform</h3>
+          <h3 style={{ fontSize: "28px", fontWeight: "bold" }}>MAS Platform</h3>
         </div>
 
         {/* Marketplace Section */}
         <div>
           <h3 style={styles.sectionHeader}>Marketplace Section</h3>
           {staticContent.slice(0, 4).map((row) => (
-            <>
-            <Link
-                        style={{ color: 'white', textDecoration: 'none', }}
-                        to={"/corporate/" + row.type}
-                        state={{
-                          data: {
-                            title: row.title,
-                            html: row.description
-                          }
-                        }}
-                      >
-                       
-                     
-            <li key={row.title} style={styles.listItem}>
-                <span>{row.title}</span>
+            <>     
+            <li key={row.title} style={styles.listItem}  onClick={() => handleClickOpen(row)}>
+                <span style={{cursor:"pointer"}}>{row.title}</span>
                 <div style={styles.divider} />
                 <div style={styles.dot} />
               </li>
-              </Link>
+             
             
             </>
+            
               
             ))}
       
+
+      <Dialog open={open} onClose={handleClose} >
+        <DialogTitle sx={{display:"flex" ,justifyContent:"space-between" ,alignItems:"center"}} color="#43005e">
+          <h1>{selectedItem?.title}</h1>
+          <div style={{fontSize:"20px",cursor:"pointer"}} onClick={handleClose}><AiOutlineClose/></div>
+          </DialogTitle>
+         <DialogContent>
+    {selectedItem?.description ? ( // Check if description exists
+      <DialogContentText>{selectedItem.description}</DialogContentText>
+    ) : (
+     <div style={{padding:"20px 60px"}}> <NoDataFound /> </div>// Render NoDataFound if no description
+    )}
+  </DialogContent>
+      </Dialog>
 
         </div> 
            
@@ -120,12 +142,14 @@ console.log("heeeee",socialLinks)
         <div >
           <h3 style={{ ...styles.sectionHeader, marginBottom: "20px" }}>Join The Community</h3>
           <div style={styles.iconsContainer} className="iconContainer">
-          <div  style={styles.iconContainer}>
-          <Link to={socialLinks[0]?.link} target="_blank" rel="noreferrer">
-              <CgFacebook  style={styles.icon}/>
+         
+          <div  style={styles.iconContainer} >
+          <Link to={socialLinks[3]?.link} target="_blank" rel="noreferrer">
+              <FaFacebook  style={styles.icon}/>
             </Link>
 
             </div>
+
 
 
             <div  style={styles.iconContainer} >
@@ -140,32 +164,19 @@ console.log("heeeee",socialLinks)
 
             <div  style={styles.iconContainer} >
           <Link to={socialLinks[2]?.link} target="_blank" rel="noreferrer">
-              <FaTwitterSquare  style={styles.icon}/>
+              <AiOutlineClose  style={styles.icon}/>
             </Link>
 
             </div>
         
             <div  style={styles.iconContainer} >
           <Link to={socialLinks[1]?.link} target="_blank" rel="noreferrer">
-              <FaYoutube  style={styles.icon}/>
+              <FaDiscord  style={styles.icon}/>
             </Link>
 
             </div>
 
-            <div  style={styles.iconContainer} >
-          <Link to={socialLinks[4]?.link} target="_blank" rel="noreferrer">
-              <SiAppstore  style={styles.icon}/>
-            </Link>
-
-            </div>
-
-
-            <div  style={styles.iconContainer} >
-          <Link to={socialLinks[4]?.link} target="_blank" rel="noreferrer">
-              <SiGoogleplay  style={styles.icon}/>
-            </Link>
-
-            </div>
+            
            
           </div>
         </div>
